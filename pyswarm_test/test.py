028c559389a90a4cea21b0913274f5ed953b7390
Radu Savutiu <rsavutiu@gmail.com>
2019-07-08 15:19:05 +0200

    nothing important

--- pyswarm_test/test.py
+++ pyswarm_test/test.py
@@ -1,46 +1,55 @@
 from pyswarm import pso
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+from matplotlib import cm
 
 def banana(x):
     x1 = x[0]
     x2 = x[1]
-    return x1**4 - 2*x2*x1**2 + x2**2 + x1**2 - 2*x1 + 5
+    return np.sqrt(x1**2 + x2**2)
 
 
-def banana2(x):
-    x1 = x[0]
-    x2 = x[1]
-    return x1**4 - 2*x2*x1**2 + x2**2 + x1**2 - 2*x1 + 5
-
-def con(x):
-    x1 = x[0]
-    x2 = x[1]
-    return [-(x1 + 0.25)**2 + 0.75*x2]
+def randrange(n, vmin, vmax):
+    '''
+    Helper function to make an array of random numbers having shape (n, )
+    with each number distributed Uniform(vmin, vmax).
+    '''
+    return (vmax - vmin) * np.random.rand(n) + vmin
 
 
 if __name__=="__main__":
-    lb = [-3, -1]
-    ub = [2, 6]
 
-    xopt, fopt = pso(banana, lb, ub, f_ieqcons=con)
+    lb = [-10, -10]
+    ub = [10, 10]
+
+    # xopt, fopt = pso(banana, lb, ub, f_ieqcons=con)
+    xopt, fopt = pso(banana, lb, ub)
     print xopt, fopt
 
     inputs_x, inputs_y, outputs_graph = [], [], []
+    n = 100
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
-    for i in range(-3, 6, 1):
-        for j in range(-3, 6, 1):
-            inputs_x.append(i)
-            inputs_y.append(j)
-            outputs_graph.append(banana(([i, j])))
+    # For each set of style and range settings, plot n random points in the box
+    # defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
 
-    X, Y = np.meshgrid(inputs_x, inputs_y)
-    Z = banana(X)
+    xs = np.arange(-10., 10., 0.1)
+    ys = np.arange(-10., 10., 0.1)
+    X, Y = np.meshgrid(xs, ys)
+    zss = np.sqrt(X**2 + Y**2)
+    # ax.plot_wireframe(X, Y, zss)
+    surf = ax.plot_surface(X, Y, zss, rstride=1, cstride=1, cmap=cm.coolwarm,
+                           linewidth=0, antialiased=False)
+    ax.set_xlim(-10, 10)
+    ax.set_ylim(-10, 10)
+    ax.set_zlim(-20, 20)
+    fig.colorbar(surf, shrink=0.5, aspect=10)
 
-    ax = Axes3D(plt.gcf())
-    ax.plot_surface(X, Y, Z)
-    ax.set_zlim(-3, 70)
-    ax.set_xlim(-3, 6)
-    ax.set_ylim(-3, 6)
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
     plt.show()
